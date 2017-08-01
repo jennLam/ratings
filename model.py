e17d15cfbd87b755fb5ca0937cab60a8a477fca8
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-01 12:54:20 -0700

    Added html templates, created registration form and routes.

--- model.py
+++ model.py
@@ -42,6 +42,13 @@ class Movie(db.Model):
     released_at = db.Column(db.DateTime, nullable=True)
     imdb_url = db.Column(db.String(150), nullable=True)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Movie movie_id=%s title=%s released_at=%s imdb_url=%s>" % (self.movie_id,
+                                               self.title, self.released_at, self.imdb_url)
+
+
 
 class Rating(db.Model):
     """Rating of ratings website."""
@@ -49,9 +56,24 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"), nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.user_id"), nullable=False)
     score = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>"
+        return s % (self.rating_id, self.movie_id, self.user_id,
+                    self.score)
 
 
 ##############################################################################
